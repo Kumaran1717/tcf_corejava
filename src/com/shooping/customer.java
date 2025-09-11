f27aa3689137258c9cf8aab3dc5caf1c3dd5ab7b
Kumaran1717 <kumaran171818@gmail.com>
2025-09-11 15:43:54 +0530

    multithread

---
+++ src/com/shooping/customer.java
@@ -0,0 +1,10 @@
+package com.shooping;
+
+public class customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/shooping/dresses.java
@@ -0,0 +1,10 @@
+package com.shooping;
+
+public class dresses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
