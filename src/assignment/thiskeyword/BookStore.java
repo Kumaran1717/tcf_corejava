58f24c28696fa0cfcec33cae0bb1b31bf6175606
Kumaran1717 <kumaran171818@gmail.com>
2025-09-11 15:46:24 +0530

    multi

--- src/assignment/thiskeyword/BookStore.java
+++ src/assignment/thiskeyword/BookStore.java
@@ -3,6 +3,6 @@ package assignment.thiskeyword;
 public class BookStore {
 
 	public void addBook(Book b) {
-        System.out.println("Book added to store: " + b.title + " by " + b.author);
+        //System.out.println("Book added to store: " + b.title + " by " + b.author);
     }
 }
